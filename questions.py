5c3faae3987ab67cd5c80f0063010a62d7df3ede
Armand <52544834+armand124@users.noreply.github.com>
2022-07-26 18:05:21 +0300

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,9 +2,6 @@
 class Fact:
  factsEarth = ['Pâmantul,numit și planeta albastră, este a treia planetă de la soare și a cincea ca masă și diametru.',"Aproximativ un sfert din suprafata pamantului este un uscat, format din continente si insule.","ceva","altceva"]
  explanationEarth = ['Diametrul este segmentul ce trece prin centrul unui cerc si are capetele pe cerc | este cel mai lung segment ce are capetele pe cerc.Masa este o marime fizica ce masoara inertia unui corp. De exemplu, cand ne masuram pe cantar si nu ne miscam suntem intr-o stare de inertie.', '404','404','404']
- questionEarth = ["Cat de grasa este mami tau?","BamBam","ciu ciu","Is bobi priti?"]
- answearModeEarth = ["Write" , "Write","AF","AF"]
- answearsEarth = ["Pog","Lol","Adevarat","Adevarat"]
 
 
  factsSolar = []
